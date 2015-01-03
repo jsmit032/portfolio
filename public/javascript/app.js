1be821b2b42d17108e1a800476410b41943eaa42
jsmit032 <jsmit032@ucr.edu>
2015-01-02 22:34:11 -0800

    used tutorial for angular to get working views

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -11,9 +11,67 @@ angular.module('Portfolio', ['ui.router'])
 			controller: 'HomeController'
 		})
 
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: 'views/posts.html',
+			controller: 'PostsCtrl'
+		})
+
 		.state('about', {
 			url: '/about',
 			templateUrl: 'views/about.html'
 		});
+
+}])
+
+.factory('posts', [function(){
+	var o = {
+		posts: []
+	};
+	return o;
+}])
+
+.controller('HomeController', ['$scope', 'posts', function($scope, posts){
+	
+	$scope.posts = posts.posts;
+
+	$scope.addPost = function(){
+		if(!$scope.title || $scope.title === '') { return; }
+
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0,
+			comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+			]
+		});
+
+		$scope.title = '';
+		$scope.link = '';
+	};
+
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	};
+
+
+
+}])
+
+.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts){
+
+	$scope.post = posts.posts[$stateParams.id];
+
+	$scope.addComment = function(){
+	  if($scope.body === '') { return; }
+	  $scope.post.comments.push({
+	    body: $scope.body,
+	    author: 'user',
+	    upvotes: 0
+	  });
+	  $scope.body = '';
+	};
 
 }]);
